89345f2a8593a52415471df340ca62d75064f400
Maxime Ichoung-Thoe <maxime.ichoung@gmail.com>
2022-05-17 15:58:23 +0200

    Finir la boucle du menubar dans le eventHandler

--- src/main/java/vue/HBoxRoot.java
+++ src/main/java/vue/HBoxRoot.java
@@ -3,11 +3,18 @@ package vue;
 import controleur.Controleur;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.control.TextArea;
+import javafx.scene.Node;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import modele.Planning;
 
-public class HBoxRoot extends HBox
+import static modele.ContantesCalendrier.MOIS;
+
+
+public class HBoxRoot extends HBox implements modele.Menu
 {
     private static Planning planning;
     private static Controleur controleur;
@@ -24,8 +31,42 @@ public class HBoxRoot extends HBox
         calendrierPane = new VBoxCalendrier();
         affichagePlannig = new DateArea();
 
+        MenuBar menuBar = new MenuBar();
+        Menu menu = new Menu(INTITULE_MENU_PLANNING);
+        menuBar.getMenus().add(menu);
+        getChildren().add(menuBar);
+        
 
-        getChildren().addAll(calendrierPane, reservationPane, affichagePlannig);
+        Node [] compenents = new Node[3];
+        compenents[0] = calendrierPane;
+        compenents[1] = reservationPane;
+        compenents[2] = affichagePlannig;
+        calendrierPane.setId("back");
+        reservationPane.setId("back");
+        affichagePlannig.setId("back");
+
+        StackPane stackPane = new StackPane(compenents);
+        stackPane.getChildren().get(0).toFront();
+
+        getChildren().add(stackPane);
+
+        //getChildren().addAll(calendrierPane, reservationPane, affichagePlannig);
+
+        for (int i = 0; i < ITEMS_MENU_PLANNING.length; i++) {
+            MenuItem menuItem = new MenuItem(ITEMS_MENU_PLANNING[i]);
+            menuItem.setUserData(i);
+            menuItem.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent actionEvent) {
+                    int data = (int)((MenuItem)actionEvent.getSource()).getUserData();
+                    while ((stackPane.getChildren().get(0).getUserData()).equals(data))
+                    {
+                        stackPane.getChildren().get(0).toFront();
+                    }
+                }
+            });
+            menu.getItems().add(menuItem);
+        }
     }
 
     public static EventHandler<ActionEvent> getControleur() {
---
+++ src/main/java/vue/Menu.java
@@ -0,0 +1,11 @@
+package modele;
+
+public interface Menu {
+    String [] ITEMS_MENU_PLANNING = {
+            "Calendrier",
+            "Réservation",
+            "Semaine"
+    };
+
+    String INTITULE_MENU_PLANNING = "Calendrier";
+}
